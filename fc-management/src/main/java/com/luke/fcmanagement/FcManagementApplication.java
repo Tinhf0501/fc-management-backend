35be7c9080cefd7c4b5ddf0b398522ee2eeb532f
Tinhf0501 <tinhnh@vnpay.vn>
2024-01-18 08:58:53 +0700

    create project

---
+++ fc-management/src/main/java/com/luke/fcmanagement/FcManagementApplication.java
@@ -0,0 +1,13 @@
+package com.luke.fcmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcManagementApplication.class, args);
+	}
+
+}
