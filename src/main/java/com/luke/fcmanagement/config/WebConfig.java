6c998e027b2f455c055cd990f5805f3f744d73a2
suongnv <suongnguyenytm2001@gmail.com>
2024-05-12 01:30:12 +0700

    config CORS

--- src/main/java/com/luke/fcmanagement/config/WebConfig.java
+++ src/main/java/com/luke/fcmanagement/config/WebConfig.java
@@ -2,11 +2,13 @@ package com.luke.fcmanagement.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.io.File;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
@@ -15,9 +17,19 @@ public class WebConfig implements WebMvcConfigurer {
     @Value("${media.saver.local.absolute-path}")
     private String resourceLocalPath;
 
+    @Value("${security.cors.url}")
+    public String[] corsUrl;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("resources/**")
                 .addResourceLocations("file:" + this.resourceLocalPath + File.separator);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(this.corsUrl)
+                .allowedMethods("GET", "POST", "DELETE", "PUT");
     }
 }
